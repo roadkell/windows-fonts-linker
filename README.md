cffaa78f6c017f0d3c585b2f70e97fe636bf8693
Kelly Roadkill <4406611+roadkell@users.noreply.github.com>
2023-09-15 15:45:50 +0500

    docs: :memo: move usage instructions to README

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 ## Windows fonts linker ##
 
 Make MS Windows fonts from an existing Windows installation available in Linux through symlinking (actually, a general purpose symlinking script).
+
+### Usage ###
+```
+linker.sh /path/to/windows/fontdir /path/to/linux/fontdir /path/to/fontlist
+```
+
+### Sources of font names ###
+
+https://wiki.archlinux.org/title/Microsoft_fonts
+https://aur.archlinux.org/packages/ttf-ms-win10-auto
+https://aur.archlinux.org/packages/ttf-ms-win11-auto
+https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=ttf-ms-win10-auto
+https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=ttf-ms-win11-auto
+https://learn.microsoft.com/en-us/typography/fonts/windows_10_font_list
+https://learn.microsoft.com/en-us/typography/fonts/windows_11_font_list
+https://en.wikipedia.org/wiki/List_of_typefaces_included_with_Microsoft_Windows
--- linker.sh
+++ linker.sh
@@ -3,19 +3,6 @@
 # Make MS Windows fonts available in Linux through symlinking
 # (actually, a general purpose symlinking script)
 # https://github.com/roadkell/windows-fonts-linker
-#
-# Usage:
-#	linker.sh /path/to/windows/fontdir /path/to/linux/fontdir /path/to/fontlist
-#
-# Sources of font names:
-#	https://wiki.archlinux.org/title/Microsoft_fonts
-#	https://aur.archlinux.org/packages/ttf-ms-win10-auto
-#	https://aur.archlinux.org/packages/ttf-ms-win11-auto
-#	https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=ttf-ms-win10-auto
-#	https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=ttf-ms-win11-auto
-#	https://learn.microsoft.com/en-us/typography/fonts/windows_10_font_list
-#	https://learn.microsoft.com/en-us/typography/fonts/windows_11_font_list
-#	https://en.wikipedia.org/wiki/List_of_typefaces_included_with_Microsoft_Windows
 
 filesdir=$(realpath -e "$1")
 linksdir=$(realpath -e "$2")
